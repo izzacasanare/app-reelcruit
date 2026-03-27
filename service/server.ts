5b152ac21d641bdfdec9f3cee600125c7ff3a908
izzacasanare <izza.casanare@mspbots.ai>
2026-03-27 22:56:18 +0800

    Update server.ts

--- service/server.ts
+++ service/server.ts
@@ -1,12 +1,13 @@
 import { Hono } from 'hono';
 import { registerJobRoutes } from './handlers/jobs.handler.ts';
+import { registerCandidateRoutes } from './handlers/candidates.handler.ts';
 
 const app = new Hono();
 
 registerJobRoutes(app);
+registerCandidateRoutes(app);
 
-// More route groups will be registered here as features are built:
-// registerCandidateRoutes(app);
+// Coming soon:
 // registerInterviewRoutes(app);
 // registerResponseRoutes(app);
 // registerReviewRoutes(app);
