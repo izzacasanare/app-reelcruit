c351f2670f6fff405958e07be01178555586d3db
izzacasanare <izza.casanare@mspbots.ai>
2026-03-27 23:41:33 +0800

    Update server.ts

--- service/server.ts
+++ service/server.ts
@@ -2,15 +2,13 @@ import { Hono } from 'hono';
 import { registerJobRoutes } from './handlers/jobs.handler.ts';
 import { registerCandidateRoutes } from './handlers/candidates.handler.ts';
 import { registerInterviewRoutes } from './handlers/interviews.handler.ts';
+import { registerReviewRoutes } from './handlers/reviews.handler.ts';
 
 const app = new Hono();
 
 registerJobRoutes(app);
 registerCandidateRoutes(app);
 registerInterviewRoutes(app);
-
-// Coming soon:
-// registerResponseRoutes(app);
-// registerReviewRoutes(app);
+registerReviewRoutes(app);
 
 export default app;
